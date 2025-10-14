043428aaf6b6d0cb7e0422d25383326961c25396
ludwig666 <63304521+ludwig666@users.noreply.github.com>
2025-10-14 19:30:04 +0900

    refactor MustCounter component layout and update button icon for clarity

--- MustCounter.js
+++ MustCounter.js
@@ -6,44 +6,83 @@ const MustCounter = {
     return { count };
   },
   template: `
-<v-number-input
-  reverse
-  controlVariant="stacked"
-  label="相手のマストカウンター枚数"
-  :max="250"
-  :min="0"
-  :model-value="4"
-  variant="solo"
-></v-number-input>
-<v-number-input
-  reverse
-  controlVariant="stacked"
-  label="相手のデッキ枚数"
-  :max="250"
-  :min="0"
-  :model-value="60"
-  variant="solo"
-></v-number-input>
-<v-number-input
-  reverse
-  controlVariant="stacked"
-  label="自分のカウンター枚数"
-  :max="250"
-  :min="0"
-  :model-value="4"
-  variant="solo"
-></v-number-input>
-<v-number-input
-  reverse
-  controlVariant="stacked"
-  label="自分のデッキ枚数"
-  :max="250"
-  :min="0"
-  :model-value="60"
-  variant="solo"
-></v-number-input>
+<v-card
+  class="ma-4 pa-4"
+  title="マストカウンター計算機"
+  text="対戦相手のフィニッシャーをすべてカウンターできるか、その確率を計算します。">
+  <v-card-text>
+    <div>マストカウンター枚数とデッキ枚数を入力してください。</div>
+    <div>自分のカウンター枚数とデッキ枚数も入力してください。</div>
+    <div>計算ボタンを押すと、相手のフィニッシャーをすべてカウンターできる確率が表示されます。</div>
+  </v-card-text>
+</v-card>
+
+<v-sheet :elevation="4" rounded class="d-flex ma-4 pa-4">
+  <v-container>
+    <v-row>
+      <h4>相手</h4>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-number-input
+          reverse
+          controlVariant="stacked"
+          label="マストカウンター枚数"
+          :max="250"
+          :min="0"
+          :model-value="4"
+          variant="solo"
+        ></v-number-input>
+      </v-col>
+      <v-col>
+        <v-number-input
+          reverse
+          controlVariant="stacked"
+          label="デッキ枚数"
+          :max="250"
+          :min="0"
+          :model-value="60"
+          variant="solo"
+        ></v-number-input>
+      </v-col>
+    </v-row>
+  </v-container>
+</v-sheet>
+
+<v-sheet :elevation="4" rounded class="d-flex ma-4 pa-4">
+  <v-container>
+    <v-row>
+      <h4>自分</h4>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-number-input
+          reverse
+          controlVariant="stacked"
+          label="カウンター枚数"
+          :max="250"
+          :min="0"
+          :model-value="4"
+          variant="solo"
+        ></v-number-input>
+      </v-col>
+      <v-col>
+        <v-number-input
+          reverse
+          controlVariant="stacked"
+          label="デッキ枚数"
+          :max="250"
+          :min="0"
+          :model-value="60"
+          variant="solo"
+        ></v-number-input>
+      </v-col>
+    </v-row>
+  </v-container>
+</v-sheet>
+
 <v-btn icon>
-  <v-icon>mdi-heart</v-icon>
+  <v-icon>mdi-calculator</v-icon>
 </v-btn>
 `,
 };
