1c54085093390e36ae274e173b455f8372c33f75
ludwig666 <63304521+ludwig666@users.noreply.github.com>
2025-10-14 15:33:19 +0900

    refactor MustCounter component and update index.html for improved layout and navigation

--- MustCounter.js
+++ MustCounter.js
@@ -6,9 +6,46 @@ const MustCounter = {
     return { count };
   },
   template: `
-    <button @click="count++">
-      You clicked me {{ count }} times.
-    </button>`,
+<v-number-input
+  reverse
+  controlVariant="stacked"
+  label="相手のマストカウンター枚数"
+  :max="250"
+  :min="0"
+  :model-value="4"
+  variant="solo"
+></v-number-input>
+<v-number-input
+  reverse
+  controlVariant="stacked"
+  label="相手のデッキ枚数"
+  :max="250"
+  :min="0"
+  :model-value="60"
+  variant="solo"
+></v-number-input>
+<v-number-input
+  reverse
+  controlVariant="stacked"
+  label="自分のカウンター枚数"
+  :max="250"
+  :min="0"
+  :model-value="4"
+  variant="solo"
+></v-number-input>
+<v-number-input
+  reverse
+  controlVariant="stacked"
+  label="自分のデッキ枚数"
+  :max="250"
+  :min="0"
+  :model-value="60"
+  variant="solo"
+></v-number-input>
+<v-btn icon>
+  <v-icon>mdi-heart</v-icon>
+</v-btn>
+`,
 };
 
 export { MustCounter };
